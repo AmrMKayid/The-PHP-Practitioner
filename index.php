9deb5a768cc684cf9bf59c3bbd6775d5b0a654b2
Amr M. Kayid <amrkayid2027@gmail.com>
2018-07-11 13:43:59 +0200

    php & html

--- index.php
+++ index.php
@@ -1,12 +1,37 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Learning</title>
+	<style>
+		
+		header {
+			background: #e3e3e3;
+			padding: 2em;
+			text-align: center;
+		}
+	</style>
 
-$greeting = "Hello, World!";
+</head>
+<body>
 
-echo $greeting;
+	<header>
+		<h1>
+			<?php 
 
-$name = "Amr M. Kayid";
+				// fetching a URL parameters from the address bar.
+				$name = $_GET['name'];
 
-echo 'Hello, $name';
-echo "Hello, $name";
-echo "Hello, {$name}";
-echo 'Hello' . $name;
+				echo "Hello, $name";
+
+			 ?>
+		</h1>	
+		
+		<!-- Same as < ? php -->
+		<!-- Function for security -->
+		<h1> <?= "Hello, " . htmlspecialchars($_GET['name']) ?></h1>
+
+	</header>
+	
+
+</body>
+</html>
