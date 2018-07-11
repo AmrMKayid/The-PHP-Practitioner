b9d19c0d342b7d7dfc064ecdc8346102f4be4684
Amr M. Kayid <amrkayid2027@gmail.com>
2018-07-11 13:33:32 +0200

    Variables

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo "Hello, World!";
+$greeting = "Hello, World!";
+
+echo $greeting;
+
+$name = "Amr M. Kayid";
+
+echo 'Hello, $name';
+echo "Hello, $name";
+echo "Hello, {$name}";
+echo 'Hello' . $name;
