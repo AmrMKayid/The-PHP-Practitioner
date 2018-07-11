008fcace2d6845fdd074d3f0209c90dacd7a31be
Amr M. Kayid <amrkayid2027@gmail.com>
2018-07-11 13:47:31 +0200

    Seperate php

--- index.php
+++ index.php
@@ -1,37 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>PHP Learning</title>
-	<style>
-		
-		header {
-			background: #e3e3e3;
-			padding: 2em;
-			text-align: center;
-		}
-	</style>
+<?php
 
-</head>
-<body>
+$greeting = "Hello, World!";
 
-	<header>
-		<h1>
-			<?php 
-
-				// fetching a URL parameters from the address bar.
-				$name = $_GET['name'];
-
-				echo "Hello, $name";
-
-			 ?>
-		</h1>	
-		
-		<!-- Same as < ? php -->
-		<!-- Function for security -->
-		<h1> <?= "Hello, " . htmlspecialchars($_GET['name']) ?></h1>
-
-	</header>
-	
-
-</body>
-</html>
+require "index.view.php";
